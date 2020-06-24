2b2ce138a7a2fcdcf82319ffad64c00d19e83306
Lee Ji Hyeong <izymwb1@naver.com>
2020-06-24 16:18:27 +0900

    modify crawler.py

--- crawler.py
+++ crawler.py
@@ -9,8 +9,9 @@ def crawler(asset_manager, url):
     except Error as e:
         print(e)
     if asset_manager == "Blackrock":
+        aum = soup.select("#keyFundFacts > div.product-data-list.data-points-en_US > div.float-left.in-left.col-totalNetAssets")[0].find("span",class_="data").string.strip()
         ytm = soup.select("#fundamentalsAndRisk > div.product-data-list.data-points-en_US > div.float-left.in-right.col-yieldToWorst")[0].find("span",class_="data").string.strip()
-        return ytm
+        return aum
     elif asset_manager == "Invesco":
         ytm = soup.find_all("div",class_="widget gray-bg stacked")[3].find_all("span",class_="pull-right")[0].string.strip()
         return ytm
