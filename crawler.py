ae5caa6b7f9714988ad2fa8902f7c3a399b4b85f
Lee Ji Hyeong <izymwb1@naver.com>
2020-06-28 21:54:30 +0900

    crawler.py modified, crawl-info.json added

--- crawler.py
+++ crawler.py
@@ -1,17 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
+import sys
+import json 
 
 def crawler(asset_manager, url):
     try :
         r = requests.get(url)
-        html = r.text
-        soup = BeautifulSoup(html, "html.parser")
+        if r.status_code == 200:
+            html = r.text
+            soup = BeautifulSoup(html, "html.parser")
+            with open('./crawl-info.json', 'r') as f:
+                crawl_info_dic = json.load(f)
+            if asset_manager == "Blackrock":
+                selector = crawl_info_dic["Blackrock"]["selectors"]                
+                aum = soup.select(selector["aum"])[0].find("span",class_="data").string.strip()
+                aum_date = soup.select(selector["aum"])[0].find("span",class_="as-of-date").string
+                ytm = soup.select(selector["ytm"])[0].find("span",class_="data").string.strip()
+                ytm_date = soup.select(selector["ytm"])[0].find("span",class_="as-of-date").string
+
+                print(aum, aum_date, ytm, ytm_date)
+                # return aum
+            # elif asset_manager == "Invesco":
+            #     ytm = soup.find_all("div",class_="widget gray-bg stacked")[3].find_all("span",class_="pull-right")[0].string.strip()
+            #     return ytm
+        else: 
+            print("html page does not exist","\nErrorcode : ",r.status_code)
+            pass
     except Error as e:
         print(e)
-    if asset_manager == "Blackrock":
-        aum = soup.select("#keyFundFacts > div.product-data-list.data-points-en_US > div.float-left.in-left.col-totalNetAssets")[0].find("span",class_="data").string.strip()
-        ytm = soup.select("#fundamentalsAndRisk > div.product-data-list.data-points-en_US > div.float-left.in-right.col-yieldToWorst")[0].find("span",class_="data").string.strip()
-        return aum
-    elif asset_manager == "Invesco":
-        ytm = soup.find_all("div",class_="widget gray-bg stacked")[3].find_all("span",class_="pull-right")[0].string.strip()
-        return ytm
+        pass
+
+if __name__ == "__main__":
+    crawler(*sys.argv[1:])
